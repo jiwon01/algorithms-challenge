65334b54c6c4ab5189e52ba06f004cdf5bef8288
Jiwon Kim <loefegoo@gmail.com>
2023-07-02 18:48:49 +0900

    [level 2] Title: 피로도, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -5,24 +5,26 @@
 answer_list를 reverse True 정렬하여 제일 큰 수를 리턴함.
  
 """
-from itertools import *
+from itertools import permutations  # 수열 사용
 
 def solution(k, dungeons):  # 현재 피로도, [최소 필요, 소모 피로]
-    answer = -1
     answer_list = []
+    # dungeons 길이만큼 리스트를 생성[0, 1, 2], 이 리스트를 dungeons의 길이 만큼 순열 생성하여 lists에 리스트 저장.
     lists = list(permutations([i for i in range(len(dungeons))], len(dungeons)))
+    #print(lists)
     
-    for idx in lists:
-        hp = k
-        result = 0
-        for i in idx:
-            if hp >= dungeons[i][0]:
-                hp -= dungeons[i][1]
-                result += 1
-            else:
-                continue
-        if result > 0:
-            answer_list.append(result)
+    for idx in lists:  # lists 원소 만큼 반복. idx = [0, 1, 2] ... 
+        hp = k  # k를 hp 변수에 매번 저장함.
+        result = 0  # 각 경우의 수에서 나온 result
+        for i in idx:  # 경우의 수가 담긴 idx(0, 1, 2) 또는 (0, 2, 1) 같이... 반복
+            if hp >= dungeons[i][0]:  # 현재 hp가 던젼의 최소로 요구하는 피로도가 충분한가?
+                hp -= dungeons[i][1]  # 던전에서 사용한 피로도를 hp에서 깎음
+                result += 1  # result +1 해줌
+            else:  # 피로도에 만족하지 못하므로 걍 다음으로
+                break
+        if result > 0:  # 위 계산 결과가 0을 초과하는가?
+            answer_list.append(result)  # answer_list에 추가
         
-    answer_list.sort(reverse=True)
-    return answer_list[0]
+    answer_list.sort(reverse=True)  # 제일 큰 값이 앞으로 오도록 정렬
+    #print(answer_list)
+    return answer_list[0]  # 제일 큰 값이 리턴됨.
