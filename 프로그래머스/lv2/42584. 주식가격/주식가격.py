2b25b9028f33ca5efb38fd5a00c1f785724c5fdc
Jiwon Kim <loefegoo@gmail.com>
2023-07-04 15:46:52 +0900

    [level 2] Title: 주식가격, Time: 76.36 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,3 +1,48 @@
+"""
+prices popleft()하여 확보한 값을 v라고 하고, result를 1이라고 하면,
+prices for로 반복하여 값이 같거나 크면 result + 1를, 작아지면 그냥 result 값을 answer에 append함.
+
+
+"""
+from collections import deque
+
+def solution(price):
+    answer = []
+    price = deque(price)
+    
+    while price:
+        v = price.popleft()
+        result = 0
+        
+        for vv in price:
+            if v <= vv:
+                result += 1
+            else:
+                result += 1
+                break
+        
+        answer.append(result)
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+이전 풀이
+
 from collections import deque
 
 def solution(prices):
@@ -14,3 +59,4 @@ def solution(prices):
                 answer_ += 1
         answer.append(answer_)
     return answer
+"""
